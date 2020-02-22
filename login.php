79bbd0689afb68087b2bd5c7648116e64afe1627
Samuel Boerner <samuel.boerner@pscs.org>
2020-02-22 05:17:15 -0800

    Initial login page and styling
    Will derive styling off the basic 'fruitbasket' theme used by RVFB

---
+++ login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Volunteer Login - Rainier Valley Food Bank</title>
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/login.css">
+  </head>
+  <body>
+    <a id="login-button" class="button" href="/backend/auth.php">(Staff) Login</a>
+  </body>
+</html>
